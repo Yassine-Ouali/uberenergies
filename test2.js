562736f6fcdb893cd64921be9ff9c02a9c0cb7fe
Yassine-Oualii <92408069+Yassine-Oualii@users.noreply.github.com>
2023-05-31 14:10:24 -0400

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,3 +1,4 @@
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -8,3 +9,24 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elementsToShow = document.querySelectorAll('.animated');
+
+  function showElements() {
+    for (var i = 0; i < elementsToShow.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = elementsToShow[i].getBoundingClientRect().top;
+      var elementHeight = elementsToShow[i].offsetHeight;
+
+      if (elementTop < windowHeight - (elementHeight / 2)) {
+        elementsToShow[i].classList.add("animate-show");
+      }
+    }
+  }
+
+  showElements();
+
+  window.addEventListener('scroll', showElements);
+});
+
